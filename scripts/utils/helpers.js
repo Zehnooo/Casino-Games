ab3af73870da6d8d3c3e2665fffb5192a1b83ba9
Zehnooo <126801501+Zehnooo@users.noreply.github.com>
2025-07-07 16:48:44 -0500

    messing with game display reset after starting the selected game

--- scripts/utils/helpers.js
+++ scripts/utils/helpers.js
@@ -1,5 +1,6 @@
 import { createDeck, dealGame } from "./deck.js";
-
+const gameDisplay = document.querySelector("#display-game");
+gameDisplay.innerHTML = ``;
 /*
 Generic helper functions
 get random number
@@ -13,7 +14,9 @@ function startGame(game) {
     game = "blackjack";
     const deck = createDeck();
     const blackJackGame = dealGame(game, deck);
-
+    gameDisplay.innerHTML = `
+    INSERT EPIC BLACKJACK GAME
+      `;
     console.log("blackjack game started", blackJackGame.deck);
     console.log(blackJackGame.playerHand);
     console.log(blackJackGame.dealerHand);
