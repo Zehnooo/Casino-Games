54c45ff4f616e283e9d4ca49e68631de70f2d24b
BuildTools <unconfigured@null.spigotmc.org>
2025-07-07 21:16:30 -0500

    added function to create buttons for dom

--- scripts/utils/domHelpers.js
+++ scripts/utils/domHelpers.js
@@ -1,3 +1,11 @@
 /*
 Dom creation, manipulation, removal, etc.
 */
+function createButton(id, text) {
+    const btn = document.createElement("button");
+    btn.id = id;
+    btn.textContent = text;
+    return btn;
+}
+
+export { createButton }
